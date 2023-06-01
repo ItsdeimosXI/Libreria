72ea0aa4b386c70e651ff53b33c81318557d0be9
deimos <salamancamanuel310@gmail.com>
2023-06-01 16:21:29 -0300

    Git

---
+++ backend/src/Bd/CategoriaDAO.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace Raiz\Bd;
---
+++ backend/src/Bd/GeneroDAO.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Raiz\Bd;
+
+use Raiz\Aux\Serializador;
+use Raiz\Bd\InterfaceDAO;
+use Raiz\Models\Genero;
+
+
+class GeneroDAO implements InterfaceDAO
+{
+
+    public static function listar(): array
+    {
+        $sql = 'SELECT * FROM genero';
+        $listagenero = ConectarBD::leer(sql: $sql);
+        $genero = [];
+        foreach ($listagenero as $Genero) {
+            $genero[] = Genero::deserializar($Genero);
+        }
+        return $genero;
+    }
+    public static function encontrarUno(string $id): ?Genero
+    {
+        $sql = 'SELECT * FROM genero WHERE id =:id;';
+        $Genero = ConectarBD::leer(sql: $sql, params: [':id' => $id]);
+        if (count($Genero) === 0) {
+           return null;
+        } else {
+            $Genero = Genero::deserializar($Genero[0]);
+            return $Genero;
+        }
+    }
+
+    public static function crear(Serializador $instancia): void
+    {
+        $params = $instancia->serializar();
+        $sql = 'INSERT INTO genero (id, descripcion, activo) VALUES (:id, :descripcion, :activo)';
+        ConectarBD::escribir(
+            sql: $sql,
+            params: [
+                ':id' => $params['id'],
+                ':descripcion' => $params['descripcion'],
+                ':activo' => $params['activo']
+            ]
+        );
+    }
+
+    public static function actualizar(Serializador $instancia): void
+    {
+        $params = $instancia->serializar();
+        $sql = 'UPDATE genero SET nombre =:nombre WHERE id=:id';
+        ConectarBD::escribir(
+            sql: $sql,
+            params: [
+                ':id' => $params['id'],
+                ':nombre' => $params['nombre'],
+            ]
+        );
+    }
+    public static function borrar(string $id)
+    {
+        $sql = 'DELETE FROM genero WHERE id=:id';
+        ConectarBD::escribir(sql: $sql, params: [':id' => $id]);
+    }
+}
