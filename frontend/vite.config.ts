43a2b121e697c93b69a3c1ec018c5aa3980c2e9e
deimos <salamancamanuel310@gmail.com>
2023-06-20 20:27:48 -0300

    si

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 8080, // you can replace this port with any port
-  }
+server:{
+  watch: {
+    usePolling: true,
+  },
+  host: true, // needed for the Docker Container port mapping to work
+  strictPort: true,
+  port: 8080, // you can replace this port with any port
+},
+plugins: [ vue()],
 })
